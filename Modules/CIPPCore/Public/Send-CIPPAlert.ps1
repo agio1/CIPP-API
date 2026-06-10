b8dd758b2772b38c8f3afbeb4e80fc866dddeae6
Scott Jenkins <98427068+scottjenkins2022@users.noreply.github.com>
2026-06-09 21:20:40 -0400

    fix: use full WEBSITE_DEPLOYMENT_ID for vault name (supports hyphenated app names)

--- Modules/CIPPCore/Public/Send-CIPPAlert.ps1
+++ Modules/CIPPCore/Public/Send-CIPPAlert.ps1
@@ -127,7 +127,7 @@ function Send-CIPPAlert {
                 return (Get-CIPPAzDataTableEntity @DevSecretsTable -Filter "PartitionKey eq '$SecretName' and RowKey eq '$SecretName'").APIKey
             }
 
-            $KeyVaultName = ($env:WEBSITE_DEPLOYMENT_ID -split '-')[0]
+            $KeyVaultName = $env:WEBSITE_DEPLOYMENT_ID
             return (Get-CippKeyVaultSecret -VaultName $KeyVaultName -Name $SecretName -AsPlainText)
         }
 
